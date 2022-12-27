16de57a1b68fadc7e8de0365b7c1f2fd3b22b2b3
Tonnie <vontonnie@gmail.com>
2022-12-27 17:23:48 +0300

    Add NetworkIncomResponseDTO.toModel() extension function

--- app/src/main/java/com/uxstate/networkincometest/data/remote/mappers/Mappers.kt
+++ app/src/main/java/com/uxstate/networkincometest/data/remote/mappers/Mappers.kt
@@ -1,39 +1,47 @@
 package com.uxstate.networkincometest.data.remote.mappers
 
 import com.uxstate.networkincometest.data.remote.dto.NetworkIncomeResponseDTO
+import com.uxstate.networkincometest.data.remote.dto.ProductDTO
 import com.uxstate.networkincometest.domain.model.ReceiptItem.Item
 import com.uxstate.networkincometest.domain.model.ReceiptItem.ReceiptItem
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 //NetworkIncomeResponseDTO to model [ReceiptItem]
 
-fun NetworkIncomeResponseDTO.toModel():ReceiptItem{
-    
+fun NetworkIncomeResponseDTO.toModel(): ReceiptItem {
+
     return ReceiptItem(
-            date =this.date.toLocalDate(),
+            date = this.date.toLocalDate(),
             customerName = this.customer.customerName,
             description = this.description,
-            items = List<Item>(this.productList.size){ i -> this.productList[i]},
-            totalPrice = "",
-            discount = "",
-            totalPayable = "",
-            balance = ""
+            items = List(this.productList.size) { i -> this.productList[i].toItem() },
+            totalPrice = this.totalAmount,
+            discount = this.discount,
+            totalPayable = this.subtotal,
+            balance = this.balanceDue
     )
-    
+
 }
 
 
-//ProductDTO to model [ReceiptItem]
+//ProductDTO to model [Item]
+fun ProductDTO.toItem(): Item {
+
+    return Item(
+            productName = this.productName,
+            productQuantity = this.productQuantity,
+            productPrice = this.productTotalPrice
+    )
+}
 
 //Extension function on String class to LocalDateTime
 
-fun String.toLocalDate():LocalDateTime{
+fun String.toLocalDate(): LocalDateTime {
 
     val pattern = "yyyy-MM-ddTHH:mm:ss"
     val dateFormatter = DateTimeFormatter.ofPattern(pattern)
-    return LocalDateTime.parse(this,dateFormatter)
+    return LocalDateTime.parse(this, dateFormatter)
 
 }
 
